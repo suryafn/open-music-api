6995d5f7b73909c5dae4fc8291ea1bf7ecbee345
DESKTOP-V5TNN31\user <surya.nainggolan@bankmandiri.co.id>
2024-07-18 20:32:54 +0700

    update the template for each service and plugin

---
+++ src/server.js
@@ -0,0 +1,60 @@
+require('dotenv').config();
+const Hapi = require('@hapi/hapi');
+const ClientError = require('./exceptions/ClientError');
+const albums = require('./api/albums');
+const AlbumsService = require('./services/AlbumsService');
+const AlbumsValidator = require('./validator/albums');
+const SongsService = require('./services/SongsService');
+const songs = require('./api/songs');
+const SongsValidator = require('./validator/songs');
+
+const init = async () => {
+  const albumsService = new AlbumsService();
+  const songsService = new SongsService();
+
+  const server = Hapi.server({
+    port: process.env.PORT,
+    host: process.env.HOST,
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+
+  await server.register([
+    {
+      plugin: albums,
+      options: {
+        service: albumsService,
+        validator: AlbumsValidator,
+      },
+    },
+    {
+      plugin: songs,
+      options: {
+        service: songsService,
+        validator: SongsValidator,
+      },
+    },
+  ]);
+
+  server.ext('onPreResponse', (request, h) => {
+    const { response } = request;
+
+    if (response instanceof ClientError) {
+      const newResponse = h.response({
+        status: 'fail',
+        message: response.message,
+      });
+      newResponse.code(response.statusCode);
+      return newResponse;
+    }
+    return h.continue;
+  });
+
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
---
+++ src/services/SongsService.js
@@ -0,0 +1,5 @@
+class SongsService {
+
+}
+
+module.exports = SongsService;
